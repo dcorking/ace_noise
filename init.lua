60d8d164f83aea8728c47e707133e07d7408e6b7
David Corking <dmc-source@dcorking.com>
2014-10-26 09:21:26 +0000

    Toggle siren on and off on punch noise block

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-print("++++ Hahahaha - hello from Dad +++++")
+print("ace_noise: ++++ Hahahaha - hello from Dad +++++")
 
 minetest.after(5,
 	       function(params)
@@ -6,25 +6,29 @@ minetest.after(5,
 	       end
 )
 
+local ace_noise_siren_handle
+local siren_state = false
+
 minetest.register_on_punchnode(
    function(pos, node, puncher)
       if node.name == "ace_noise:noise"
       then
-	 minetest.chat_send_all("EMERGENCY SIREN ACTIVATED!")
-         handle = minetest.sound_play("ace_noise_civildefense", {gain = 1.0,})
-         minetest.register_on_punchnode(
-            function(pos, node, puncher)
-               if node.name == "ace_noise:noise"
-               then
-                  minetest.chat_send_all("All Clear. All Clear.")
-                  minetest.sound_stop(handle)
-               end
-         end)
+         if not siren_state
+         then
+            minetest.chat_send_all("EMERGENCY SIREN ACTIVATED!")
+            ace_noise_siren_handle = minetest.sound_play("ace_noise_civildefense", {gain = 1.0,})
+         else
+            minetest.chat_send_all("All Clear. All Clear.")
+            minetest.sound_stop(ace_noise_siren_handle)
+         end
+         siren_state = not siren_state
+         print("ace_noise: ace_noise_siren_handle = " .. ace_noise_siren_handle)
+         print("ace_noise: siren_state = " .. tostring(siren_state))
       end
    end
 )
 
 minetest.register_node("ace_noise:noise", {
-	description = "Noise",
-	tiles = {"ace_noise_noise_block.png"},
+                          description = "Noise",
+                          tiles = {"ace_noise_noise_block.png"},
 })
